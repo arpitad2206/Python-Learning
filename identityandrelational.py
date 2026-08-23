c43d5dfaae7e8b9c4ecdb7a86eb33b6816bec587
Arpita Dudhanikar <arpitadudhanikar@gmail.com>
2026-08-23 12:07:58 +0530

    Add files via upload

---
+++ identityandrelational.py
@@ -0,0 +1,12 @@
+a=[]
+b=[]
+c=a
+print(id(a))
+print(id(a))
+print(id(c))
+print(a==b)
+print(a is b)
+print(a is c)
+
+
+
---
+++ list_operations.py
@@ -0,0 +1,17 @@
+#list operations
+cricketers=['rohit','virat','msd','surya']
+print(cricketers)
+#append
+cricketers.append('tilak')
+print(cricketers)
+#insert
+cricketers.insert(1,'bumrah')
+print(cricketers)
+#remove
+cricketers.remove('bumrah')
+print(cricketers)
+#pop
+cricketers.pop(4)
+print(cricketers)
+#slicing
+print(cricketers[0:3])
