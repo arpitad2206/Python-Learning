d2a8189616c3956313f1e5e8a6bd8453e0a81773
Arpita Dudhanikar <arpitadudhanikar@gmail.com>
2026-08-23 13:12:54 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,12 @@
+fruits=['apple','kiwi','mango','don']
+for i in fruits:
+    print(i)
+    #looping in strings
+for i in "computer":
+    print(i)
+#break
+cricketer=['surya','rohit','virat','msd']
+for i in cricketer:
+    print(i)
+    if i=='virat':
+        break
---
+++ function_inpython.py
@@ -0,0 +1,4 @@
+#parameter passing
+def info(name,name2):
+    print("iam fan of",name, "&",name2)
+info("rohit","virat")
